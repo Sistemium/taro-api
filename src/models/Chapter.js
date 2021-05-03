012182ec1e712022f5d8ec813f0682669ba8dd0a
Alexander Levin <alevin.ru@gmail.com>
2021-05-03 19:40:46 +0300

    Chapter.name

--- src/models/Chapter.js
+++ src/models/Chapter.js
@@ -3,6 +3,7 @@ import { CodeString } from './LocalizedString';
 export default {
   collection: 'Chapter',
   schema: {
+    name: String,
     title: CodeString,
     description: CodeString,
     info: CodeString,
